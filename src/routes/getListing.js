60a3871917cf2c6ea67d350fef4be537789caf56
Peter <>
2023-08-13 23:42:02 +0200

    upd: fetching data from local file

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,10 +1,17 @@
 import { fakeListings } from './fake-data';
+import { Boom } from '@hapi/boom';
 
 export const getListingRoute = {
   method: 'GET',
   path: '/api/listings/{id}',
   handler: (req, h) => {
     const id = req.params.id;
-    return fakeListings.find((listing) => listing.id === id);
+    const listing = fakeListings.find((listing) => listing.id === id);
+
+    return listing;
+
+    // not working with postman // DUNNO why?
+    // if (!listing) throw Boom.notFound('Listing not found');
+    // return listing;
   },
 };
