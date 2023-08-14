d151e141d698cbdfad99086e4121add62af2802c
Peter <>
2023-08-14 17:18:52 +0200

    upd: individual-user-req

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 import { addViewToListingRouter } from './addViewToListing';
+import { getUserListingsRoute } from './getUserListings';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
 
-export default [getAllListingsRoute, getListingRoute, addViewToListingRouter];
+export default [
+  getAllListingsRoute,
+  getListingRoute,
+  addViewToListingRouter,
+  getUserListingsRoute,
+];
