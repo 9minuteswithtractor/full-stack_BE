56337c2e3f90b90e8bec8218a59fc11c333d67a9
Peter <>
2023-08-14 18:42:18 +0200

    feat: add-new-listing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { addViewToListingRouter } from './addViewToListing';
+import { createNewListingRoute } from './createNewListing';
 import { getUserListingsRoute } from './getUserListings';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
 
 export default [
   getAllListingsRoute,
+  createNewListingRoute,
   getListingRoute,
   addViewToListingRouter,
   getUserListingsRoute,
